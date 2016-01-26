77fcce0491c9b2e65c427e6a00dfc397abb3d0a7
Mike Pitre <mpitre@mikes-macbook-pro.local>
2016-01-26 15:37:57 -0600

    printing new restaurant text fields, toggling been here button color, added save button

--- FoodPin/AddRestaurantController.swift
+++ FoodPin/AddRestaurantController.swift
@@ -11,7 +11,14 @@ import UIKit
 class AddRestaurantController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var restaurantName: UITextField!
+    @IBOutlet weak var restaurantType: UITextField!
+    @IBOutlet weak var restaurantLocation: UITextField!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
     
+    var isVisited = true
+    let foodPinPink = UIColor(red: 236.0/255, green: 102.0/255, blue: 111.0/255, alpha: 1.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +58,35 @@ class AddRestaurantController: UITableViewController, UIImagePickerControllerDel
         dismissViewControllerAnimated(true, completion: nil)
     }
 
+    @IBAction func saveButtonPressed(sender: AnyObject) {
+        if restaurantName.text != "" && restaurantType.text != "" && restaurantLocation.text != "" {
+            print("Name: \(restaurantName.text))")
+            print("Type: \(restaurantType.text))")
+            print("Location: \(restaurantLocation.text))")
+            print("isVisited: \(isVisited)")
+            
+            dismissViewControllerAnimated(true, completion: nil)
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Oops!", message: "One of the fields has been left blank. Please fill all the fields out.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func beenHereButtonPressed(sender: UIButton) {
+        if sender == yesButton {
+            isVisited = true
+            yesButton.backgroundColor = foodPinPink
+            noButton.backgroundColor = UIColor.lightGrayColor()
+        } else if sender == noButton {
+            isVisited = false
+            yesButton.backgroundColor = UIColor.lightGrayColor()
+            noButton.backgroundColor = foodPinPink
+        }
+    }
+    
     
 
 }
